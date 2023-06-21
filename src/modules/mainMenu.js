bdbf2c976e3409416d7799c1a84a9e8c95ff5105
SergeyEvtush <evtushru208@gmail.com>
2023-06-21 11:12:13 +0200

    скролл на блоки сайта с главного меню

--- src/modules/mainMenu.js
+++ src/modules/mainMenu.js
@@ -4,7 +4,8 @@ export const mainMenu = () => {
 	const menu = document.querySelector('.top-menu');
 	menu.addEventListener('click', (e) => {
 		e.preventDefault();
-		if (target.matches('a')) {
+		const target = e.target;
+		if (target.closest('a')) {
 			console.log(target);
 			scrollingToView(target, { block: "center", inline: "nearest", behavior: "smooth" });
 		}
